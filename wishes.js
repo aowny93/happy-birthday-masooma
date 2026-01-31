e1d58f0c941a59a71a84dffcd75efe0ea743d887
aowny93 <aowny93@gmail.com>
2026-01-31 04:54:17 +0000

    Update wishes.js

--- wishes.js
+++ wishes.js
@@ -1,11 +1,12 @@
 const wishes = [
-    {
-        name: "Aown",
-        message: "Hi Masooma, it's Aown, your husband. Today is so special because my life partner, my everything, is born. ❤️"
-    },
-    {
-        name: "Hadi",
-        message: "Happy birthday Masooma bahbi! Mola apko lambo zindgayi isi tarah hasti muskarti mere bahi k sath rehy akhari sans tak insha'Allah. Apko har wish puri ho gayi Mola Ghazi AS k sadqay. Jo jo apky dil mei hay sab pora ho ga. Once again happy birthday! Treat de 😂😂"
-    },
-    // add more wishes here
+  {
+    name: "Aown",
+    message: "Hi Masooma, it's Aown, your husband. Today is so special because my life partner, my everything, is born. ❤️"
+  },
+  {
+    name: "Hadi",
+    message: "Happy birthday Masooma bahbi! Mola apko lambo zindgayi isi tarah hasti muskarti mere bahi k sath rehy akhari sans tak insha'Allah. Apko har wish puri ho gayi Mola Ghazi AS k sadqay. Jo jo apky dil mei hay sab pora ho ga. Once again happy birthday! Treat de 😂😂"
+  }
+  // Add more wishes here, make sure each wish is separated by a comma
 ];
+
