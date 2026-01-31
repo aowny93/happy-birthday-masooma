7a383fc8a905c48d5cf0ca0e7af594d9aa4c724e
aowny93 <aowny93@gmail.com>
2026-01-31 04:10:10 +0000

    Update wishes.js

--- wishes.js
+++ wishes.js
@@ -1,5 +1,5 @@
 const wishes = [
-  { name: "Aown", message: "Happy Birthday, my love! You are my everything ❤️" },
+  { name: "Aown", message: ""My dearest Masooma, even though miles separate us, my heart is always with you. You are my world, my dream, my everything. Happy Birthday, my love — I promise our love will grow stronger every day, and soon we’ll be together, making every moment as beautiful as you make my life. ❤️"" },
   { name: "Ali", message: "Wishing you a day filled with laughter and joy!" },
   { name: "Sara", message: "May all your dreams come true this year!" },
   { name: "John", message: "Hope your birthday is as amazing as you are!" },
